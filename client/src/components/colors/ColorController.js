893356103d7d04d199d639f9575448e6ac248c0e
KinkMustard <alec2001@gmail.com>
2018-04-22 16:57:32 -0600

    made the website actually look good

--- client/src/components/colors/ColorController.js
+++ client/src/components/colors/ColorController.js
@@ -6,10 +6,24 @@ import PropTypes from "prop-types";
 import classNames from "classnames";
 import { withStyles } from "material-ui/styles";
 import Button from "material-ui/Button";
-import colorFields from "./colorFields";
+import { colorFields } from "./colorFields";
 import { GET_COLOR, CHANGE_COLOR, UPDATE_COLOR } from "../../Events";
+import Icon from "material-ui/Icon";
+import grey from "material-ui/colors/grey";
+import lightBlue from "material-ui/colors/lightBlue";
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle
+} from "material-ui/Dialog";
+import Slide from "material-ui/transitions/Slide";
 
 const endPoint = process.env.NODE_ENV === "development" ? "http://192.168.1.97:5000" : "/";
+
+function Transition(props) {
+  return <Slide direction="up" {...props} />;
+}
 
 const styles = theme => ({
   button: {
@@ -17,6 +31,13 @@ const styles = theme => ({
   },
   input: {
     display: "none"
+  },
+  iconHover: {
+    margin: theme.spacing.unit * 2,
+    color: grey[300],
+    "&:hover": {
+      color: lightBlue[300]
+    }
   }
 });
 
@@ -26,7 +47,8 @@ class ColorController extends React.Component {
 
     this.state = {
       color: "white",
-      socket: null
+      socket: null,
+      open: false
     };
   }
   componentWillMount() {
@@ -54,9 +76,17 @@ class ColorController extends React.Component {
     socket.emit(CHANGE_COLOR, color);
     console.log("set color", color);
   };
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   renderFields() {
     const { classes } = this.props;
-    return _.map(colorFields, ({ color }) => {
+    return _.map(colorFields, ({ color, shade }) => {
       return (
         <Button
           id={color}
@@ -64,11 +94,9 @@ class ColorController extends React.Component {
           variant="raised"
           color={"primary"}
           className={classes.button}
-          onClick={() => this.setColor(color)}
-          style={{ backgroundColor: color }}
-        >
-          {color}
-        </Button>
+          onClick={() => this.setColor(shade)}
+          style={{ backgroundColor: shade }}
+        />
       );
     });
   }
@@ -89,8 +117,55 @@ class ColorController extends React.Component {
           }}
         >
           {this.renderFields()}
-          <p>{this.state.nice}</p>
+          <Button
+            variant="fab"
+            color="primary"
+            aria-label="add"
+            className={classes.button}
+            onClick={this.handleClickOpen}
+          >
+            <Icon>info_outline</Icon>
+          </Button>
+          <p
+            style={{
+              textAlign: "center",
+              color: "#BDBDBD",
+              fontFamily: "Roboto",
+              marginTop: "10%"
+            }}
+          />
         </div>
+        <Dialog
+          open={this.state.open}
+          transition={Transition}
+          keepMounted
+          onClose={this.handleClose}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle id="alert-dialog-slide-title" style={{ textAlign: "center" }}>
+            {"About this website"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description" style={{ textAlign: "center" }}>
+              This is a real time website that uses web sockets.<br />Try pulling up this website on
+              another tab or on your phone and see what happens when you change the color.
+              <br />
+              <br />
+              Made by Alec Chen
+              <br />
+              <br />
+              <a href="https://github.com/KinkMustard/colorchanger" target="_blank">
+                Github
+              </a>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
       </React.Fragment>
     );
   }
--- client/src/components/colors/colorFields.js
+++ client/src/components/colors/colorFields.js
@@ -1,7 +1,28 @@
-export default [
-  { color: "red" },
-  { color: "green" },
-  { color: "blue" },
-  { color: "yellow" },
-  { color: "magenta" }
+import baseRed from "material-ui/colors/red";
+import baseOrange from "material-ui/colors/orange";
+import baseLime from "material-ui/colors/yellow";
+import baseGreen from "material-ui/colors/green";
+import baseTeal from "material-ui/colors/teal";
+import baseBlue from "material-ui/colors/blue";
+import basePurple from "material-ui/colors/purple";
+import baseGrey from "material-ui/colors/grey";
+
+export const Red = baseRed.A700;
+export const Orange = baseOrange.A700;
+export const Lime = baseLime.A700;
+export const Green = baseGreen.A700;
+export const Teal = baseTeal.A700;
+export const Purple = basePurple.A700;
+export const Blue = baseBlue.A700;
+export const White = baseGrey["50"];
+export const Black = basePurple["900"];
+
+export const colorFields = [
+  { color: "red", shade: Red },
+  { color: "orange", shade: Orange },
+  { color: "lime", shade: Lime },
+  { color: "green", shade: Green },
+  { color: "teal", shade: Teal },
+  { color: "blue", shade: Blue },
+  { color: "purple", shade: Purple }
 ];
